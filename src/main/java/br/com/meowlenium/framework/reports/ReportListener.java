32c713ee7cda2e2b6270964dc5bf8db7b78d5354
Cleverson Sampaio <clevsampaio@outlook.com.br>
2020-12-22 13:15:57 -0300

    Corrigindo a falta de sincronização para gerar e extrair o reporte

--- src/main/java/br/com/meowlenium/framework/reports/ReportListener.java
+++ src/main/java/br/com/meowlenium/framework/reports/ReportListener.java
@@ -15,36 +15,30 @@ import java.io.File;
 
 public class ReportListener implements ITestListener {
     @Override
-    public void onStart(ITestContext context) {
+    public synchronized void onStart(ITestContext context) {
         ExtentService.getInstance().setAnalysisStrategy(AnalysisStrategy.CLASS);
         ExtentService.getInstance().setSystemInfo("Ambiente", Property.getEnv());
         ExtentService.getInstance().setSystemInfo("Sistema", System.getProperty("os.name"));
     }
 
     @Override
-    public void onFinish(ITestContext context) {
-        try {
-            // TODO: É necessário aguardar ao menos 3 segundos para o reporte seja gerado e extraído
-            Thread.sleep(3000);
+    public synchronized void onFinish(ITestContext context) {
+        String email = System.getProperty("property.email");
+        String report = System.getProperty("property.report");
 
-            String email = System.getProperty("property.email");
-            String report = System.getProperty("property.report");
+        if (!StringUtils.isEmpty(email) && email.equals("true")) {
+            File fileReport = new File(Directory.REPORT_FOLDER + File.separator + "report.html");
+            ReportModel reportExtract = Report.extract(fileReport);
+            String mailBody = Mailer.builder(reportExtract);
+            Mailer.send(Property.get("email", "email.to"), mailBody, fileReport);
+        }
 
-            if (!StringUtils.isEmpty(email) && email.equals("true")) {
-                File fileReport = new File(Directory.REPORT_FOLDER + File.separator + "report.html");
-                ReportModel reportExtract = Report.extract(fileReport);
-                String mailBody = Mailer.builder(reportExtract);
-                Mailer.send(Property.get("email", "email.to"), mailBody, fileReport);
-            }
-
-            if (!StringUtils.isEmpty(report) && report.equals("true")) {
-                File fileReport = new File(Directory.REPORT_FOLDER + File.separator + "report.html");
-                ReportModel reportExtract = Report.extract(fileReport);
-                String path = "/api/project/" + Property.get("report", "token");
-                Rest rest = new Rest(Property.get("report", "url"));
-                rest.post(path, 200, Report.json(reportExtract));
-            }
-        } catch (InterruptedException ignored) {
+        if (!StringUtils.isEmpty(report) && report.equals("true")) {
+            File fileReport = new File(Directory.REPORT_FOLDER + File.separator + "report.html");
+            ReportModel reportExtract = Report.extract(fileReport);
+            String path = "/api/project/" + Property.get("report", "token");
+            Rest rest = new Rest(Property.get("report", "url"));
+            rest.post(path, 200, Report.json(reportExtract));
         }
     }
 }
